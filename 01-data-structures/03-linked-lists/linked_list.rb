22af059a73eadb2a1bc5db9b533824a95156ba5f
joecaintic <joecaintic@yahoo.com>
2016-06-28 19:35:42 -0400

    linked list rb file complete

--- 01-data-structures/03-linked-lists/linked_list.rb
+++ 01-data-structures/03-linked-lists/linked_list.rb
@@ -1,42 +1,102 @@
-require 'node'
+require_relative 'node'
 
 class LinkedList
-  attr_accessor :head
-  attr_accessor :tail
-
-  # This method creates a new `Node` using `data`, and inserts it at the end of the list.
-  def add_to_tail(node)
-      node = Node.new(data)
-      @tail = node
-  end
-
-  # This method removes the last node in the lists and must keep the rest of the list intact.
-  def remove_tail  
-      LinkedList.delete(@tail)
-  end
-
-  # This method prints out a representation of the list.
-  def print
-      print LinkedList
-  end
-
-  # This method removes `node` from the list and must keep the rest of the list intact.
-  def delete(node)
-      LinkedList.delete(node)
-  end
-
-  # This method adds `node` to the front of the list and must set the list's head to `node`.
-  def add_to_front(node)
-      node = Node.new(data)
-      @head = node
-  end
-
-  # This method removes and returns the first node in the Linked List and must set Linked List's head to the second node.
-  def remove_front
-      temp = @head
-      @head = @head.next
-      LinkedList.delete(temp)
-      return temp
-  end
+    attr_accessor :head
+    attr_accessor :tail
+    
+    # This method creates a new `Node` using `data`, and inserts it at the end of the list.
+    def add_to_tail(data)
+        if @head === nil && @tail === nil
+            @head = data
+            @tail = @head
+            @tail.next = nil
+        elsif @tail === nil
+            @tail = data
+        else
+            @tail.next = data
+            @tail = data
+        end
+    end
+    
+    # This method removes the last node in the lists and must keep the rest of the list intact.
+    def remove_tail
+        current_code = @head
+        if @tail === false
+            nil
+        elsif current_code === @tail
+            @tail.next = nil
+            @tail = nil
+            current_code = nil
+            @head.next = nil
+            @head = nil
+        elsif current_code.next === @tail
+            @tail = current_code
+            @tail.next = nil
+        else
+            current_code = current_code.next
+        end
+    end
+    
+    # This method prints out a representation of the list.
+    def print
+        current_node = @head
+        while current_node != nil
+            puts current_node.data
+            current_node = current_node.next
+        end
+    end
+    
+    # This method removes 'node' from the list and must keep the rest of the list intact.
+    def delete(node)
+        if node === false
+            nil
+        elsif @head === node && @tail === node
+            @tail.next = nil
+            @tail = nil
+            @head.next = nil
+            @head = nil
+        elsif @head === node
+            @head = @head.next
+        else
+            previous = @head
+            current = @head.next
+            until current === nil
+                if current === node && current === @tail
+                    @tail = previous
+                    break
+                elsif current === node && current != @tail
+                    previous.next = current.next
+                    break
+                else
+                    previous = current
+                    current = current.next
+                end
+            end
+        end
+    end
+    
+    # This method adds `node` to the front of the list and must set the list's head to `node`.
+    def add_to_front(node)
+        if @head === nil && @tail === nil
+            @head = node
+            @tail = @head
+            @tail.next = nil
+        else
+            temp = @head
+            @head = node
+            @head.next = temp
+        end
+    end
+    
+    # This method removes and returns the first node in the Linked List and must set Linked List's head to the second node.
+    def remove_front
+        if @head === nil
+            return @head
+        else
+            temp = @head
+            @head = @head.next
+            return temp
+        end
+    end
     
 end
--- 01-data-structures/06-trees/binary_tree/binary_search_tree_spec.rb
+++ 01-data-structures/06-trees/binary_tree/binary_search_tree_spec.rb
@@ -1,43 +1,53 @@
 include RSpec
 
-RSpec.describe AvlTree, type: Class do
-  let (:tree) { AvlTree.new() }
-  let (:root) { Node.new("Donnie Darko", 85) }
+require_relative 'binary_search_tree'
+
+RSpec.describe BinarySearchTree, type: Class do
+  let (:root) { Node.new("The Matrix", 87) }
+
+  let (:tree) { BinarySearchTree.new(root) }
+  let (:pacific_rim) { Node.new("Pacific Rim", 72) }
+  let (:braveheart) { Node.new("Braveheart", 78) }
+  let (:jedi) { Node.new("Star Wars: Return of the Jedi", 80) }
+  let (:donnie) { Node.new("Donnie Darko", 85) }
+  let (:inception) { Node.new("Inception", 86) }
+  let (:district) { Node.new("District 9", 90) }
+  let (:shawshank) { Node.new("The Shawshank Redemption", 91) }
+  let (:martian) { Node.new("The Martian", 92) }
   let (:hope) { Node.new("Star Wars: A New Hope", 93) }
   let (:empire) { Node.new("Star Wars: The Empire Strikes Back", 94) }
-  let (:jedi) { Node.new("Star Wars: Return of the Jedi", 80) }
-  let (:the_martian) { Node.new("The Martian", 92) }
-  let (:pacific_rim) { Node.new("Pacific Rim", 72) }
-  let (:inception) { Node.new("Inception", 86) }
-  let (:braveheart) { Node.new("Braveheart", 78) }
-  let (:shawshank) { Node.new("The Shawshank Redemption", 91) }
-  let (:matrix) { Node.new("The Matrix", 87) }
-  let (:district) { Node.new("District 9", 90) }
   let (:mad_max_2) { Node.new("Mad Max 2: The Road Warrior", 98) }
 
   describe "#insert(data)" do
-    it "inserts a new root if it is null" do
-      tree.insert(nil, root)
-      expect(tree.find(nil, "Donnie Darko")).to_not be_nil
+    it "properly inserts a new node as a left child" do
+      tree.insert(root, pacific_rim)
+      expect(root.left.title).to eq "Pacific Rim"
     end
 
-    it "inserts a new node as a left child" do
-      tree.insert(root, jedi)
-      expect(root.left.title).to eq "Star Wars: Return of the Jedi"
-    end
-
-    it "inserts a new node as a left-left child" do
-      tree.insert(root, jedi)
+    it "properly inserts a new node as a left-left child" do
+      tree.insert(root, braveheart)
       tree.insert(root, pacific_rim)
       expect(root.left.left.title).to eq "Pacific Rim"
     end
 
-    it "inserts a new node as a right child" do
-      tree.insert(root, empire)
-      expect(root.right.title).to eq "Star Wars: The Empire Strikes Back"
+    it "properly inserts a new node as a left-right child" do
+      tree.insert(root, donnie)
+      tree.insert(root, inception)
+      expect(root.left.right.title).to eq "Inception"
     end
 
-    it "inserts a new node as a right-right child" do
+    it "properly inserts a new node as a right child" do
+      tree.insert(root, district)
+      expect(root.right.title).to eq "District 9"
+    end
+
+    it "properly inserts a new node as a right-left child" do
+      tree.insert(root, hope)
+      tree.insert(root, martian)
+      expect(root.right.left.title).to eq "The Martian"
+    end
+
+    it "properly inserts a new node as a right-right child" do
       tree.insert(root, empire)
       tree.insert(root, mad_max_2)
       expect(root.right.right.title).to eq "Mad Max 2: The Road Warrior"
@@ -45,56 +55,121 @@ RSpec.describe AvlTree, type: Class do
   end
 
   describe "#find(data)" do
-    it "finds a node by it's title" do
-      tree.insert(root, empire)
-      tree.insert(root, mad_max_2)
-      expect(tree.find(root, mad_max_2.title).title).to eq "Mad Max 2: The Road Warrior"
-    end
-
-    it "hanldes nil gracefully" do
+    it "handles nil gracefully" do
       tree.insert(root, empire)
       tree.insert(root, mad_max_2)
       expect(tree.find(root, nil)).to eq nil
     end
+
+    it "properly finds a left node" do
+      tree.insert(root, pacific_rim)
+      expect(tree.find(root, pacific_rim.title).title).to eq "Pacific Rim"
+    end
+
+    it "properly finds a left-left node" do
+      tree.insert(root, braveheart)
+      tree.insert(root, pacific_rim)
+      expect(tree.find(root, pacific_rim.title).title).to eq "Pacific Rim"
+    end
+
+    it "properly finds a left-right node" do
+      tree.insert(root, donnie)
+      tree.insert(root, inception)
+      expect(tree.find(root, inception.title).title).to eq "Inception"
+    end
+
+    it "properly finds a right node" do
+      tree.insert(root, district)
+      expect(tree.find(root, district.title).title).to eq "District 9"
+    end
+
+    it "properly finds a right-left node" do
+      tree.insert(root, hope)
+      tree.insert(root, martian)
+      expect(tree.find(root, martian.title).title).to eq "The Martian"
+    end
+
+    it "properly finds a right-right node" do
+      tree.insert(root, empire)
+      tree.insert(root, mad_max_2)
+      expect(tree.find(root, mad_max_2.title).title).to eq "Mad Max 2: The Road Warrior"
+    end
   end
 
   describe "#delete(data)" do
+    it "handles nil gracefully" do
+      expect(tree.delete(root, nil)).to eq nil
+    end
 
+    it "properly deletes a left node" do
+      tree.insert(root, hope)
+      tree.delete(root, hope.title)
+      expect(tree.find(root, hope.title)).to be_nil
+    end
+
+    it "properly deletes a left-left node" do
+      tree.insert(root, braveheart)
+      tree.insert(root, pacific_rim)
+      tree.delete(root, pacific_rim.title)
+      expect(tree.find(root, pacific_rim.title)).to be_nil
+    end
+
+    it "properly deletes a left-right node" do
+      tree.insert(root, donnie)
+      tree.insert(root, inception)
+      tree.delete(root, inception.title)
+      expect(tree.find(root, inception.title)).to be_nil
+    end
+
+    it "properly deletes a right node" do
+      tree.insert(root, district)
+      tree.delete(root, district.title)
+      expect(tree.find(root, district.title)).to be_nil
+    end
+
+    it "properly deletes a right-left node" do
+      tree.insert(root, hope)
+      tree.insert(root, martian)
+      tree.delete(root, martian.title)
+      expect(tree.find(root, martian.title)).to be_nil
+    end
+
+    it "properly deletes a right-right node" do
+      tree.insert(root, empire)
+      tree.insert(root, mad_max_2)
+      tree.delete(root, mad_max_2.title)
+      expect(tree.find(root, mad_max_2.title)).to be_nil
+    end
   end
 
   describe "#printf" do
-    expected_output = "Donnie Darko: 85\nStar Wars: Return of the Jedi: 80\nInception: 86\nBraveheart: 78\nThe Matrix: 87\nPacific Rim: 72\nDistrict 9: 90\nThe Shawshank Redemption: 91\nThe Martian: 92\nStar Wars: A New Hope: 93\nStar Wars: The Empire Strikes Back: 94\nMad Max 2: The Road Warrior: 98\n"
      specify {
-       tree.insert(nil, root)
+       expected_output = "The Matrix: 87\nStar Wars: Return of the Jedi: 80\nStar Wars: A New Hope: 93\nPacific Rim: 72\nInception: 86\nThe Martian: 92\nStar Wars: The Empire Strikes Back: 94\nBraveheart: 78\nThe Shawshank Redemption: 91\nMad Max 2: The Road Warrior: 98\nDistrict 9: 90\n"
        tree.insert(root, hope)
        tree.insert(root, empire)
        tree.insert(root, jedi)
-       tree.insert(root, the_martian)
+       tree.insert(root, martian)
        tree.insert(root, pacific_rim)
        tree.insert(root, inception)
        tree.insert(root, braveheart)
        tree.insert(root, shawshank)
-       tree.insert(root, matrix)
        tree.insert(root, district)
        tree.insert(root, mad_max_2)
-
        expect { tree.printf }.to output(expected_output).to_stdout
      }
 
      specify {
-       tree.insert(nil, root)
+       expected_output = "The Matrix: 87\nBraveheart: 78\nMad Max 2: The Road Warrior: 98\nPacific Rim: 72\nInception: 86\nDistrict 9: 90\nStar Wars: Return of the Jedi: 80\nThe Shawshank Redemption: 91\nThe Martian: 92\nStar Wars: The Empire Strikes Back: 94\nStar Wars: A New Hope: 93\n"
        tree.insert(root, mad_max_2)
        tree.insert(root, district)
-       tree.insert(root, matrix)
        tree.insert(root, shawshank)
        tree.insert(root, braveheart)
        tree.insert(root, inception)
        tree.insert(root, pacific_rim)
-       tree.insert(root, the_martian)
+       tree.insert(root, martian)
        tree.insert(root, jedi)
        tree.insert(root, empire)
        tree.insert(root, hope)
-
        expect { tree.printf }.to output(expected_output).to_stdout
      }
   end
