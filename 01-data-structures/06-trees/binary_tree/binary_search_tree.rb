c38c08381cfea284b06664e6c8cb0ebbaa53371e
joecaintic <joecaintic@yahoo.com>
2016-11-16 13:04:14 -0500

    initial work for assignment 6

--- 01-data-structures/06-trees/binary_tree/binary_search_tree.rb
+++ 01-data-structures/06-trees/binary_tree/binary_search_tree.rb
@@ -3,13 +3,28 @@ require_relative 'node'
 class BinarySearchTree
 
   def initialize(root)
+      @root = root
   end
 
   def insert(root, node)
+      if root.rating >= node.rating
+         root.left.nil? ? root.left = node : self.insert(root.left, node)
+      else
+         root.right.nil? ? root.right = node : self.insert(root.right, node)
+      end
   end
 
   # Recursive Depth First Search
   def find(root, data)
+      return nil if data.nil?
+      if root.rating > data
+         find(root.left, data)
+      elsif root.rating < data
+         find(root.right, data)
+      else
+         return data.title
+      end
+      
   end
 
   def delete(root, data)
@@ -18,4 +33,9 @@ class BinarySearchTree
   # Recursive Breadth First Search
   def printf(children=nil)
   end
+
+#for delete, you got to figure out how to shift things so the children of what is deleted isn't lost ("parent" must be replaced by another parent, must still follow left-right semantics). There is an algorhithm that can be implemented that satsifies the criteria.
+
+#should find and delete based on rating. SO data ends up being rating (is rating bigger or smaller than root, and then progress from there)
+
 end
--- 01-data-structures/06-trees/binary_tree/node.rb
+++ 01-data-structures/06-trees/binary_tree/node.rb
@@ -5,5 +5,9 @@ class Node
   attr_accessor :right
 
   def initialize(title, rating)
+      self.title = title
+      self.rating = rating
+      @left = nil
+      @right = nil
   end
 end
